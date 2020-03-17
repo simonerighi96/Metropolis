67cd8712a2aa16c5ffe490fbfce1fdbeb9ce2970
ImMorpheus <ImMorpheus@users.noreply.github.com>
2020-03-17 20:47:43 +0100

    Fix admin JoinCommand errors
    Fix an issue with the admin command failing with full towns or
    event cancellation

--- src/main/java/me/morpheus/metropolis/commands/admin/town/JoinCommand.java
+++ src/main/java/me/morpheus/metropolis/commands/admin/town/JoinCommand.java
@@ -3,9 +3,12 @@ package me.morpheus.metropolis.commands.admin.town;
 import me.morpheus.metropolis.Metropolis;
 import me.morpheus.metropolis.api.command.AbstractPlayerCommand;
 import me.morpheus.metropolis.api.command.args.parsing.MinimalInputTokenizer;
+import me.morpheus.metropolis.api.data.citizen.CitizenData;
+import me.morpheus.metropolis.api.data.citizen.CitizenKeys;
 import me.morpheus.metropolis.api.rank.Ranks;
 import me.morpheus.metropolis.api.town.Town;
 import me.morpheus.metropolis.api.command.args.MPGenericArguments;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -29,12 +32,12 @@ class JoinCommand extends AbstractPlayerCommand {
     }
     @Override
     protected CommandResult process(Player source, CommandContext context) throws CommandException {
-        Town town = context.requireOne("town");
-        boolean success = town.accept(source.getUniqueId(), Ranks.CITIZEN);
+        final Town town = context.requireOne("town");
+        final CitizenData cd = Sponge.getDataManager().getManipulatorBuilder(CitizenData.class).get().create();
+        cd.set(CitizenKeys.TOWN, town.getId());
+        cd.set(CitizenKeys.RANK, Ranks.CITIZEN);
+        source.offer(cd);
 
-        if (!success) {
-            return CommandResult.empty();
-        }
         return CommandResult.success();
     }
 }
